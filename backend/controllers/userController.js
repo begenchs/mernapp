2b39d48cb2ad655dfb8e3d02843ec3e014171e9d
Bega <60026940+begenchs@users.noreply.github.com>
2022-06-17 03:33:59 +0200

    JWT

---
+++ backend/controllers/userController.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// @description Register new user
+// @route       POST /api/users
+// @access      Public
+const registerUser = asyncHandler(async (req, res) => {
+    
+    
+    res.json({message: 'Register User'})
+})
+
+// @description Authent a user
+// @POST        POST /api/users/login
+// @access      Public
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({message: 'Login User'})
+})
+
+// @description Get user data
+// @route       Get /api/users/me
+// @access      Public
+const getMe = asyncHandler(async (req, res) => {
+    res.json({message: 'User data display'})
+})
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
